41dfee824f1c2901225e092b0f5b8e619419a5e6
Henning 'haenno' Beier <haenno@web.de>
2023-10-30 18:29:13 +0100

    add print of args to hello world

--- 1-helloWorld/main.rs
+++ 1-helloWorld/main.rs
@@ -1,3 +1,13 @@
+use std::env;
+
 fn main() {
     println!("Hello, world!");
+
+    println!("Given args are following: ");
+    let args: Vec<String> = env::args().collect();
+
+    for arg in args.iter() {
+        // first arg is the name of the program and path if used in call
+        println!("{}", arg);
+    }
 }
